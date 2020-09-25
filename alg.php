115becb17874fd69547c1aa3c50e09939f0da0fa
天王州アテネ <qcmc@qcminecraft.com>
2020-09-25 12:26:35 +0000

    Add ALG.php

---
+++ alg.php
@@ -0,0 +1,27 @@
+<?php
+DEFINE("API_URL", "");
+
+$nowQueue = json_decode(file_get_contents(API_URL . "/api/queue"));
+if($nowQueue->count == 0){
+
+}
+function addQueue(){
+    send_post(API_URL . "/api/queue/items/add?expression=path+starts+with+%22%2F%22+order+by+path+asc&shuffle=false&clear=true&playback=start");
+}
+
+function send_post($url, $post_data="k=v;") {
+ 
+    $postdata = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => 'Content-type:application/x-www-form-urlencoded',
+            'content' => $postdata,
+            'timeout' => 15 * 60
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+ 
+    return $result;
+}
